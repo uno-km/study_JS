2945e6ec3c4f33b2b1aea72b84cdb238b97fc51f
uno-kim <zhfldk014745@naver.com>
2022-01-23 01:22:30 +0900

    leaning useState and install reacte-router-dom

--- src/main/webapp/bookreactjs/router-test/src/App.js
+++ src/main/webapp/bookreactjs/router-test/src/App.js
@@ -1,16 +1,33 @@
-import React, {useEffect} from 'react';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
 export default function App() {
-  useEffect(()=>{
-    window.onpopstate = function(event){
-      console.log(`location : ${document.location}, state : ${event.state}`);
-    };
-  }, []);
-  return (
-    <div className="App">
-      <button onClick={()=> window.history.pushState('v1','','/page1')}>page1</button>
-      <button onClick={()=> window.history.pushState('v2','','/page2')}>page2</button>
-    </div>
-  );
-};
+    const [pageName, setPageName] = useState('');
+    useEffect(() => {
+        window.onpopstate = event => {
+            setPageName(event.state);
+        };
+    }, []);
+    function onClick1() {
+        const pageName = 'page1';
+        window.history.pushState(pageName, '', '/page1');
+        setPageName(pageName);
+    }
+    function onClick2() {
+        const pageName = 'page2';
+        window.history.pushState(pageName, '', '/page2');
+        setPageName(pageName);
+    }
+    return (
+        <div>
+            <button onClick={onClick1}>page1</button>
+            <button onClick={onClick2}>page2</button>
+            {!pageName && <Home/>}
+            {pageName === 'page1' && <Page1/>}
+            {pageName === 'page2' && <Page2/>}
+        </div>
+    );
+}
+
+function Home() {return <h2>Please click button.</h2>;}
+function Page1() {return <h2>This is page 1.</h2>;}
+function Page2() {return <h2>This is page 2.</h2>;}
